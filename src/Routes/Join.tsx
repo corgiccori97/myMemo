8f1a0a68e1aa2110fefbedaf964c1228989eb82c
corgiccori97 <tkdgmlgg7@gmail.com>
2023-12-08 18:50:35 +0900

    모달 css 수정

--- src/Routes/Join.tsx
+++ src/Routes/Join.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface UserInfo {
     email: string;
@@ -44,17 +44,30 @@ function Join() {
 
     return (
         <>
+        <button 
+        className="flex items-center space-x-2 mx-auto text-gray-800 mt-[10%] mb-2">
+            <svg fill="none" 
+            className="w-6 h-6"
+            stroke="currentColor" 
+            strokeWidth="1.5"
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg" 
+            aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+            </svg>
+            <Link to="/">홈으로 돌아가기</Link>
+        </button>
         <form 
-        className="w-1/4 mx-auto  bg-white py-10 text-xl"
+        className="w-1/4 mx-auto p-10 text-xl text-center bg-slate-300 bg-opacity-40"
         onSubmit={handleSubmit(onSubmit)}
         name="userInfo">
             <h1 className="font-extrabold text-gray-800">회원가입하고 내 메모를 저장하세요</h1>
             <span className="font-body text-2xl">JOIN US!</span>
             <hr />
-            <div className="flex flex-col">
+            <div className="flex flex-col space-y-4 text-start">
                 {/* 이메일 */}
                 <label
-                className="font-body text-yellow-300 mt-2">
+                className="font-body text-yellow-400 mt-2">
                     EMAIL
                     <br />
                     <input {...register("email", {
@@ -62,12 +75,11 @@ function Join() {
                     })}
                     type="email" 
                     name="email"
-                    className="text-gray-600 text-sm cols-span-2"
-                    // placeholder="이메일" 
+                    className="text-gray-600 text-sm cols-span-2 rounded-xl pl-2 w-3/4"
                     />
                 </label>
                 {/* 비밀번호 */}
-                <label className="font-body text-yellow-300">
+                <label className="font-body text-yellow-400">
                     PASSWORD
                     <br />
                     <input {...register("password", {
@@ -76,13 +88,13 @@ function Join() {
                     })}
                     type="password"
                     name="password"
-                    className="text-gray-800"
+                    className="text-gray-800 rounded-xl pl-2 center w-3/4"
                     placeholder="********" 
                     /> 
                 </label>
                 {errors.password && errors.password.type === "minLength" && <span className="text-red-600 text-xs">8자 이상으로 입력해주세요</span>}
                 {/* 비밀번호 확인 */}
-                <label className="font-body text-yellow-300">
+                <label className="font-body text-yellow-400">
                     PASSWORD CONFIRM
                     <br />
                     <input {...register("passwordConfirm", {
@@ -90,14 +102,15 @@ function Join() {
                     })} 
                     type="password" 
                     name="passwordConfirm"
-                    className="text-gray-800"
+                    className="text-gray-800 rounded-xl pl-2 w-3/4"
                     placeholder="********"
                     /> 
                 </label>
                 { watch("password") !== watch("passwordConfirm") ? <span className="text-red-600 text-xs">비밀번호가 일치하지 않습니다.</span> : ""}
             </div>
             <button 
-            type="submit">제출</button>
+            type="submit"
+            className="mt-3 inline-block font-bold px-28 hover:text-yellow-400 transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">제출</button>
         </form>
         </>
     );
--- src/Routes/SignIn.tsx
+++ src/Routes/SignIn.tsx
@@ -52,20 +52,30 @@ function SignIn() {
 
     return (
         <>
-        <button>
+        <button 
+        className="flex items-center space-x-2 mx-auto text-gray-800 mt-[10%] mb-2">
+            <svg fill="none" 
+            className="w-6 h-6"
+            stroke="currentColor" 
+            strokeWidth="1.5"
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg" 
+            aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+            </svg>
             <Link to="/">홈으로 돌아가기</Link>
         </button>
         <form 
-        className="w-1/2 mx-auto"
+        className="w-1/4 mx-auto p-10 text-xl text-center bg-slate-300 bg-opacity-40"
         onSubmit={handleSubmit(onSubmit)}
         name="loginInfo">
             <h1 className="font-extrabold text-gray-800">이메일, 비밀번호로 로그인하세요</h1>
             <span className="font-body text-2xl">SIGN IN!</span>
             <hr />
-            <div className="flex flex-col">
+            <div className="flex flex-col text-start">
                 {/* 이메일 */}
                 <label
-                className="font-body text-yellow-300 mt-2">
+                className="font-body text-yellow-400 mt-2">
                     EMAIL
                     <br />
                     <input {...register("email", {
@@ -73,12 +83,12 @@ function SignIn() {
                     })}
                     type="email" 
                     name="email"
-                    className="text-gray-600 text-sm cols-span-2 underline underline-offset-4"
+                    className="text-gray-600 text-sm cols-span-2 underline underline-offset-4 rounded-xl"
                     // placeholder="이메일" 
                     />
                 </label>
                 {/* 비밀번호 */}
-                <label className="font-body text-yellow-300">
+                <label className="font-body text-yellow-400 mt-2">
                     PASSWORD
                     <br />
                     <input {...register("password", {
@@ -86,12 +96,13 @@ function SignIn() {
                     })}
                     type="password"
                     name="password"
-                    className="text-gray-800 border"
+                    className="text-gray-800 border rounded-xl"
                     /> 
                 </label>
             </div>
             <button 
-            type="submit">제출</button>
+            type="submit"
+            className="mt-3 inline-block font-bold px-28 hover:text-yellow-400 transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">제출</button>
             <br />
             { currentState === "Wrong" ? <span className="text-red-600 text-xs">존재하지 않는 유저이거나 비밀번호가 틀렸어요!</span> : <></> }
         </form>
