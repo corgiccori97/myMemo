368a02cdfbd8c25e99e9d3c8a72c8c08214025a9
corgiccori97 <tkdgmlgg7@gmail.com>
2023-12-10 20:13:50 +0900

    add+apply stamp-effect css class

--- src/Components/Add.tsx
+++ src/Components/Add.tsx
@@ -18,7 +18,7 @@ export default function Addbtn(props:propsInfo) {
         <button
         type="button"
         onClick={() => setAdd(prev => !prev)}
-        className="rounded-full px-8 pt-2.5 text-4xl font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] my-auto font-body flex-none"
+        className="rounded-full px-8 pt-2.5 text-4xl font-medium uppercase leading-normal my-auto font-body flex-none stamp-effect"
         data-modal-target="addModal"
         data-modal-toggle="addModal"
         >
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ReactTypingEffect from 'react-typing-effect';
 import NotebookModal from './Components/NotebookModal';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -19,6 +19,16 @@ function Home() {
         localStorage.setItem('title', title);
     };
     const [add, setAdd] = useState(false);
+    
+    const navigate = useNavigate();
+    const onClick = () => {
+        if (isAuthenticated) {
+            setAdd(prev => !prev);
+        } else {
+            alert("로그인하고 노트북을 추가해 보세요!");
+            navigate("/signin");
+        };
+    };
 
     // 노트북 데이터 불러오기
     useEffect(() => {
@@ -46,26 +56,20 @@ function Home() {
         <div className='mt-[10%]'>
         <button
         type="button"
-        onClick={() => setAdd(prev => !prev)}
-        className="rounded-full px-8 pt-2.5 text-4xl font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] my-auto font-body flex-none"
+        onClick={onClick}
+        className="rounded-full px-8 pt-2.5 text-4xl font-medium uppercase leading-normal my-auto font-body flex-none stamp-effect"
         >
             Add NOTEBOOK here
         </button>
-        { isAuthenticated ?  (
-            <NotebookModal 
-            usage='add'
-            isOpen={add}
-            onClose={() => setAdd(false)}
-            />
-        ) : (
-            <>
-            <span>로그인하고 노트북을 추가해 보세요!</span>
-            </>
-        )}
+        <NotebookModal 
+        usage='add'
+        isOpen={add}
+        onClose={() => setAdd(false)}
+        />
         <div className="flex flex-wrap mx-auto border border-dashed rounded-[50%] w-1/2 border-gray-400 justify-center body px-15 py-8">
             {notebooks.map((notebook) => (
-                <div className='bg-white m-2 p-2 rounded-xl cursor-pointer hover:font-extrabold'>
-                    <img src={`./public/${notebook.thumbnail}`} alt='' />
+                <div className='m-2 p-2 rounded-xl cursor-pointer hover:font-extrabold stamp-effect'>
+                    <img src={`assets/background.JPG`} alt='' />
                     <Link
                     key={notebook.notebook_id} 
                     to={`/notebook/${notebook.notebook_id}`} onClick={() => saveTitle(notebook.notebook_name)}>{notebook.notebook_name}</Link>
